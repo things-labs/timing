0ecce39a8dbe73c9eb15b97371b19b89ec9d47e9
mo <thinkgo@aliyun.com>
2020-02-28 21:55:15 +0800

    fix interface

--- interface.go
+++ interface.go
@@ -12,14 +12,3 @@ type JobFunc func()
 func (sf JobFunc) Run() {
 	sf()
 }
-
-// Submit goroutine pool interface
-type Submit interface {
-	Submit(job Job)
-}
-
-// NopSubmit empty struct implement Submit interface
-type NopSubmit struct{}
-
-// Submit implement Submit interface
-func (NopSubmit) Submit(Job) {}
